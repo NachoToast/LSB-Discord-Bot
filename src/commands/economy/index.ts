baebe3edf3775e889b4499d8c4d65a17540d16da
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-14 17:46:56 +1300

    Add raise command

--- src/commands/economy/index.ts
+++ src/commands/economy/index.ts
@@ -1,6 +1,7 @@
 import { balance, baltop, setbalance } from './balance';
 import pay from './pay';
 import portfolio from './portfolio';
+import raise from './raise';
 import work from './work';
 
-export default [balance, pay, portfolio, setbalance, baltop, work];
+export default [balance, pay, portfolio, setbalance, baltop, work, raise];
---
+++ src/commands/economy/raise.ts
@@ -0,0 +1,12 @@
+import Command, { CommandParams } from '../../client/Command';
+
+class Raise implements Command {
+    public name: string = 'raise';
+    public description: string = 'See if you can get a raise';
+
+    public async execute({ message }: CommandParams) {
+        message.channel.send(`No raise for you`);
+    }
+}
+
+export default new Raise();
