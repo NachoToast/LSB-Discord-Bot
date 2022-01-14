16ca79e7d308bdeeb7e4a26914cdd2431d48a8e6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-14 21:33:13 +1300

    custom portfolio image for nathaniel

--- src/commands/economy/portfolio.ts
+++ src/commands/economy/portfolio.ts
@@ -40,7 +40,11 @@ class Portfolio implements Command {
             .setColor('#32CD32')
             .setTitle(`💰 ${targetedUser.user.username}'s Portfolio`)
             .setDescription(description.join('\n'))
-            .setThumbnail(targetedUser.user.avatarURL() || '');
+            .setThumbnail(
+                targetedUser.user.id === '239562978037465088'
+                    ? 'https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec'
+                    : targetedUser.user.avatarURL() || '',
+            );
 
         if (user.transactions.length) {
             let shownTransactions = '';
